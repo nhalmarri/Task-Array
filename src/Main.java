b065b8b2c0ea4949c0fa464ae4e0d7b3526dcd9c
Nora <nhalmarri64@gmail.com>
2024-11-03 14:40:06 +0300

    task 3 done

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+import java.util.List;
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+
+        //Step 1: Create an array to hold five different weather temperatures.
+        //int [] temperatures = new int [5];
+
+        //Step 2: Initialize the array with five temperature values of your choice.
+
+        //temperatures[0] = 25 ;
+
+        int [] temperatures = {25,-10,38,100,-196};
+
+
+        // Step 3: Change the temperature at the third position (index 2) to a new value.
+        temperatures[2] = 38 ;
+
+        //Step 4: Use a loop to display all the temperatures in your array.
+
+
+        for(int i =0 ; i<temperatures.length;i++){
+            System.out.println(temperatures[i]);
+        }
+
+
+
+        }
+    }
