355b2324a8634e5dae0e755011ca52e246644cba
Nora <nhalmarri64@gmail.com>
2024-11-03 14:43:36 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
-import java.util.ArrayList;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 
-import java.util.List;
-import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
